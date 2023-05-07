04e6f86301946b98f232eeb76da8da6bd743917b
Roman Shevyakov <roman@mgstr.ru>
2023-05-07 17:48:38 +0600

    feat: complete 4th task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,22 @@
+const path = require('path');
+const fs = require('fs/promises');
+const sourceDir = path.join(__dirname, 'files');
+const targetDir = path.join(__dirname, 'files-copy');
 
+cloneDir();
+
+async function cloneDir() {
+  await fs.mkdir(targetDir, {recursive: true});
+  const fileList = await fs.readdir(sourceDir);
+  const cloneList = await fs.readdir(targetDir);
+
+  cloneList.forEach(async (file) => {
+    if (!fileList.includes(file)) {
+      await fs.unlink(path.join(targetDir, file));
+    }
+  });
+
+  fileList.forEach(async (file) => {
+    await fs.copyFile(path.join(sourceDir, file), path.join(targetDir, file));
+  });
+}
